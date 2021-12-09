4c68f0d974d2aa54a4ec2eef11a5527d65d916d0
Rajan kumar <krcpr007@gmail.com>
2021-12-09 15:21:34 +0530

    intial commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div className='container bg-success'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eum voluptatem aut sapiente necessitatibus minus dolor vitae eius saepe et! Accusamus ipsa veniam atque laborum optio amet quod enim minima.
+            Voluptatibus quis praesentium distinctio modi, nesciunt, omnis eius excepturi quam porro, a delectus placeat voluptate architecto blanditiis vitae debitis repellat laboriosam eum illo hic asperiores numquam? Rerum similique facilis deleniti?
+            Non ab saepe porro architecto sint eius aliquam perferendis fugiat, cumque asperiores in id velit commodi eaque beatae aliquid dignissimos voluptatum corporis voluptas reprehenderit quisquam similique molestias repellendus enim. Possimus?
+            Officia incidunt ipsam quas amet ab necessitatibus voluptate explicabo minima repellat nemo laboriosam, at, facilis fuga itaque excepturi culpa, nulla sunt sit. Numquam accusamus autem qui officia hic eum sapiente.
+            Dicta fugit cum reiciendis repellat, nisi, cupiditate eius iusto, natus quis incidunt ratione sint odit explicabo possimus? Possimus autem enim repellendus facere ratione doloribus sit beatae, molestiae nostrum odio minus!
+            Molestias saepe ea in ipsum quam quas! Rem numquam expedita quaerat! Excepturi maxime, incidunt quisquam nihil quam esse fuga voluptates pariatur eligendi libero expedita fugit quis quae natus dolores architecto?
+            Hic in tenetur adipisci blanditiis quas, pariatur eveniet, eum beatae labore explicabo dolorem nobis nemo laboriosam minus voluptatibus molestias, ex dolore aperiam sit quae cum numquam. Modi velit alias voluptatum?
+            Necessitatibus, vitae commodi vel quo rerum, minima dolores soluta mollitia ipsam dolore aliquam tenetur deleniti ab dicta. Dolor quidem, provident sint ex, cupiditate iure facere harum voluptatum dolorem possimus nam.
+            
+        </div>
+    )
+}
+
+export default Main
