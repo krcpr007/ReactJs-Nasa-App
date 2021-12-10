d6fd49cd180b5e7f23e3c5b091c05d3acb6ce104
Rajan kumar <krcpr007@gmail.com>
2021-12-10 11:02:21 +0530

    Footer we have to improove

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+          
+         <div className='footer mt-2 bg-dark text-white text-center'>
+             <h1>Nasa Application</h1>
+         </div>
+    )
+}
+
+export default Footer
