439108b6ed1ef76f9b866eefc59a6e5613eab34b
Rajan kumar <krcpr007@gmail.com>
2021-12-10 14:58:50 +0530

    lets deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Main from './components/Main';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Apod from "./components/Apod";
+import About from "./components/About";
+import Mars from "./components/Mars";
 function App() {
   return (
     <>
-    <Navbar/>
-    <Main/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/apod" element={<Apod />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/mars" element={<Mars/>}/>
+      </Routes>
+      <Footer />
     </>
   );
 }
