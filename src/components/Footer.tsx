95e8b95ceb38a1af90b4f4b760d2099dd9c2e791
Rajan kumar <krcpr007@gmail.com>
2021-12-10 15:00:01 +0530

    footer improoved

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
           
-         <div className='footer mt-2 bg-dark text-white text-center'>
+         <div className='footer fixed-bottom bg-dark text-white text-center'>
              <h1>Nasa Application</h1>
          </div>
     )
