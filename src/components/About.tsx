7db6e0e462f3372d5361f686a97e5967d862c7c8
Rajan kumar <krcpr007@gmail.com>
2021-12-10 11:04:14 +0530

    mars and about page created

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className='container-lg bg-danger'>
+            <h1>this is about page</h1>
+        </div>
+    )
+}
+
+export default About
