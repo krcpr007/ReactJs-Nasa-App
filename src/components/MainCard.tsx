d3bb9d8d055d301f88991478a44ccdc068c0fab7
Rajan kumar <krcpr007@gmail.com>
2021-12-11 13:30:09 +0530

    enhanced

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 interface cardDetails {
+  imgUrl:string;
   title: string;
   cardText: string;
   link:string; 
+  btnText:string;
 }
 function MainCard(props:cardDetails) {
   return (
     <div className="card mx-4 my-3" style={{ width: "15rem" }}>
       <img
-        src="https://source.unsplash.com/1600x900/daily?astronomy,earth"
+        src={props.imgUrl}
         className="card-img-top"
         alt="..."
       />
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
         <p className="card-text">
-          Here you can see Astronomy Picture of the Day (APOD).
-          Its means the picture taken by nasa of our universe by today
+         {props.cardText}
         </p>
         <Link to={props.link} className="btn btn-primary">
-          See picture
+          {props.btnText}
         </Link>
       </div>
     </div>
